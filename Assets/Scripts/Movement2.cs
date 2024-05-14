7c5eb9e31f72f8b0c764d9b8e58d7d9ec18755e5
Izzy039 <isaac.perezb1993@gmail.com>
2024-05-13 22:47:46 -0600

    Movimiento y salto corregidos
    El movimiento y el salto están funcionando, la cámara ya está funcionando y colisiona con el piso.

--- Assets/Scripts/Movement2.cs
+++ Assets/Scripts/Movement2.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterMovement : MonoBehaviour
+public class Movement2 : MonoBehaviour
 {
-    [Header("Parámetros generales")]
     public CharacterController controller;
     public Transform cam;
+
+    //Variables de movimiento
+    [Header("Parámetros de movimiento")]
     public float speed = 6f;
-    public float smoothAngle = 0.1f;
+    public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
 
-    [Header("Parámetros de gravedad")]
-    public float gravity = 9.81f;
+    //Variables de gravedad
+    [Header("Configuración de gravedad")]
+    public float gravity = -9.81f;
     Vector3 velocity;
 
     //Variables de salto
@@ -21,29 +24,36 @@ public class CharacterMovement : MonoBehaviour
     public float maxJumpHeight;
     public bool isGrounded;
 
-    // Update is called once per frame
     void Update()
     {
+        //Para obtener los ejes horizontal y vertical del input
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
+        //Usando WASD y flechas como input, asigna valores a los ejes X y Y, luego se normalizan para evitar acelerar si se presionan dos teclas simultáneamente
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        //Asigna valor a la gravedad y se multiplica por tiempo
+        velocity.y += gravity * Time.deltaTime;
+        //Asigna valor al booleano
         isGrounded = controller.isGrounded;
 
         //Aplica una fuerza en Y si se está en el suelo, para garantizar que el CharacterController detecte la colisión correctamente
         if (isGrounded && velocity.y < 0)
         {
-            velocity.y = -10f;
-            Debug.Log("On the ground");
+            velocity.y = -0.9f;
         }
 
         if (direction.magnitude >= 0.1f)
         {
+            //Atan calcula la rotación del modelo para poder mirar a donde apunta el mouse, luego lo convierte a grados usando Rad2Deg. Luego suma el ángulo de la cámara para mirar en esa dirección
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, smoothAngle);
+            //Suaviza el giro del personaje al cambiar de dirección
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            //Cambia la rotación del personaje al cambiar de dirección
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            //Permite que el jugador se mueva a donde apunta la cámara
             Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            //Instrucción de movimiento, multiplica dirección normalizada * velocidad * tiempo, para hacer la velocidad independiente del framerate
             controller.Move(moveDirection.normalized * speed * Time.deltaTime);
-            
         }
 
         //Instrucción para aplicar la gravedad, se multiplica por tiempo de nuevo para asignar el valor al cuadrado.
